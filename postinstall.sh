7e4558a6826f9d3078b468c0cdd4c2ee9ada4a3e
Simon Selg <simon@selg.me>
2017-02-21 16:32:13 +0100

    feat(yarn): fix yarn compatibility

--- postinstall.sh
+++ postinstall.sh
@@ -2,5 +2,4 @@
 
 VERSION=$(node --eval "console.log(require('./node_modules/electron/package.json').version);")
 cd node_modules/leveldown
-HOME=~/.electron-gyp
-../.bin/node-gyp rebuild --target=$VERSION --arch=x64 --dist-url=https://atom.io/download/atom-shell
+HOME=~/.electron-gyp ../node-gyp/bin/node-gyp.js rebuild --target=$VERSION --arch=x64 --dist-url=https://atom.io/download/atom-shell
